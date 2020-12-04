bf4606982c41072a29593f3595639337de714faa
Qdzia <46885229+Qdzia@users.noreply.github.com>
2020-12-04 14:00:48 +0100

    Create welcome screen layout
    Add directory and empty navigation file
    Empty implamentation of that fragment

---
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/WelcomeScreen.kt
@@ -0,0 +1,32 @@
+package com.example.teacherassistant
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fr_welcome_screen.*
+
+class WelcomeScreen : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fr_welcome_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        buttonToClassList.setOnClickListener {
+//                view->view.findNavController().navigate(R.id.action_welcomeScreen_to_classList)
+//        }
+    }
+
+    companion object { fun newInstance()=WelcomeScreen()}
+}
